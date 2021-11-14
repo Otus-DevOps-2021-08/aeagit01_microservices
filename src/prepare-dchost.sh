7c37170ee2150d64e4979628c74befb147e213fb
Evgeny Alpeyev <krotpk@gmail.com>
2021-11-15 02:23:25 +0300

    docker 3 complite #1

---
+++ src/prepare-dchost.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+eval $(docker-machine env docker-host)
+
+docker pull mongo:latest
+docker build -t krotpk/post:1.0 ./post-py
+docker build -t krotpk/comment:1.0 ./comment
+docker build -t krotpk/ui:1.0 ./ui
+
+docker network create reddit
+docker run -d --network=reddit --network-alias=base_pst --network-alias=msg_data mongo:latest
+docker run -d --network=reddit --network-alias=sendpost krotpk/post:1.0
+docker run -d --network=reddit --network-alias=msg_data krotpk/comment:1.0
+docker run -d --network=reddit -p 9292:9292 krotpk/ui:1.0
+
+open -a "Google Chrome" http://51.250.9.249:9292

7043dea9f73a36366194c3e81f4f332c892aa6e4
Evgeny Alpeyev <krotpk@gmail.com>
2021-12-16 00:26:40 +0300

    monitoring-1 change dir structure #1

---
+++ docker/docker-monolith/infra/ansible/ti.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python ../dinventory.py --list
+#terraform-inventory --list $@ ../terraform/
---
+++ docker/docker-monolith/infra/terraform/modules/ycimg/variables.tf
@@ -0,0 +1,5 @@
+variable "source_image_family" {
+    description = "source image family name"
+    default = "ubuntu-1604-lts"
+
+}
